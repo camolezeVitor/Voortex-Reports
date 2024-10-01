8437e2e21391f9fa2be175ca8431fc6579f1e027
VitorHugoCD <vitordelantonia@gmail.com>
2024-10-01 15:31:52 -0300

    feat: report member

---
+++ lib/members/report-member.ts
@@ -0,0 +1,24 @@
+import { Member } from "../proto/member";
+
+export type ReportProps = Member & {
+    name: string;
+    downloadable?: boolean;
+    presetStyles?: Map<string, Partial<CSSStyleDeclaration>>
+};
+
+export class ReportMember implements Member {
+    id: string = "REPORT";
+    name: string;
+    children: Array<Member>;
+    styleSheet?: Partial<CSSStyleDeclaration>;
+    downloadable?: boolean;
+    presetStyles?: Map<string, Partial<CSSStyleDeclaration>>
+
+    constructor(props: ReportProps) {
+        this.children = props.children;
+        this.styleSheet = props.styleSheet;
+        this.name = props.name;
+        this.downloadable = true || props.downloadable;
+        this.presetStyles = props.presetStyles;
+    } 
+}
